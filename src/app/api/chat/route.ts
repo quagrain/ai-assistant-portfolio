97714b6bf537e1f2cf0d2375a82c33e03c08cf4c
quagrain <victorquagraine9@gmail.com>
2025-03-02 23:51:40 +0000

    Included web page context

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,7 +1,10 @@
 import { ChatOpenAI } from "@langchain/openai";
 import { LangChainStream, OpenAIStream, StreamingTextResponse } from "ai";
 import { ChatCompletionMessageParam } from "ai/prompts";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { ChatPromptTemplate, PromptTemplate } from "@langchain/core/prompts";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { createRetrievalChain } from "langchain/chains/retrieval";
+import { getVectorStore } from "@/lib/astradb";
 
 export async function POST(req: Request) {
   try {
@@ -15,14 +18,36 @@ export async function POST(req: Request) {
       modelName: "gpt-3.5-turbo",
       streaming: true,
       callbacks: [handlers],
+      verbose: true,
     });
 
     const prompt = ChatPromptTemplate.fromMessages([
-      ["system", "Answer questions sarcastically"],
+      [
+        "system",
+        "You're a chatbot for a personal porfolio site. Impersonate the website owner." +
+          "Answer the user's questions based on the context below. " +
+          "When nexessary, provide links to the pages with information on the topic from the given context" +
+          "Format your messages in markdown.\n\n" +
+          "Context:\n{context}",
+      ],
       ["user", "{input}"],
     ]);
 
     const chain = prompt.pipe(chatModel);
-    chain.invoke({ input: currentMessageContent });
+    const combineDocsChain = await createStuffDocumentsChain({
+      llm: chatModel,
+      prompt,
+      documentPrompt: PromptTemplate.fromTemplate(
+        "Page URL: {url}\n\nPage content:\n{page_content}",
+      ),
+      documentSeparator: "\n--------\n",
+    });
+
+    const retriever = (await getVectorStore()).asRetriever(4);
+    const retrieverChain = await createRetrievalChain({
+      combineDocsChain,
+      retriever,
+    });
+    retrieverChain.invoke({ input: currentMessageContent });
   } catch (error) {}
 }
