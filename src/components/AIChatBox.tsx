842074712de80c46dbe349e62da2c26d691619ee
quagrain <victorquagraine9@gmail.com>
2025-03-04 13:40:40 +0000

    Fixed user message visibility

--- src/components/AIChatBox.tsx
+++ src/components/AIChatBox.tsx
@@ -5,6 +5,9 @@ import Link from "next/link";
 import { useEffect, useRef } from "react";
 import ReactMarkdown from "react-markdown";
 
+// TODO: Replace deprecated useChat
+// import { useChat } from "@ai-sdk/react";
+
 interface AIChatBoxProps {
   open: boolean;
   onClose: () => void;
@@ -42,7 +45,7 @@ export default function AIChatBox({ open, onClose }: AIChatBoxProps) {
     <div
       className={cn(
         "bottom-0 right-0 z-50 w-full max-w-[500px] p-1 xl:right-36",
-        open ? "fixed" : "hidden",
+        open ? "fixed" : "hidden"
       )}
     >
       <button onClick={onClose} className="mb-1 mx-auto block">
@@ -126,14 +129,14 @@ function ChatMessage({ message: { role, content } }: ChatMessageProps) {
     <div
       className={cn(
         "mb-3 flex items-center",
-        isAiMessage ? "me-5 justify-start" : "ms-5 justify-end",
+        isAiMessage ? "me-5 justify-start" : "ms-5 justify-end"
       )}
     >
       {isAiMessage && <Bot className="mr-2 flex-none" />}
       <div
         className={cn(
           "rounded-md border px-3 py-2",
-          isAiMessage ? "bg-background" : "bg-foreground",
+          isAiMessage ? "bg-background" : "bg-foreground text-secondary"
         )}
       >
         <ReactMarkdown
